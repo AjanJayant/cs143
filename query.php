4a368dd967007065e100ebc37481feb4cafdd1b8
Ajan Jayant <ajanj@ucla.edu>
2013-01-18 23:20:16 -0800

    1/17/2013

---
+++ query.php
@@ -0,0 +1,39 @@
+<html>
+
+<head><title>CS143 Project 1B Demo</title></head>
+
+<body>
+
+<p> TA: Please do not "TEST" your (especially, complicated) quries in this demo website. The loading of quries might kill the server. 
+
+Also, the machine running this website a has VERY different setting from your virutal machine, for example, the memory size, so testing your quries in this page is meaningless. 
+
+Since we grade your projects in a virtual machine. Please test your queries (especially queries with sub-queries) at your own machine. </p>
+
+Type an SQL query in the following box:
+
+<p>
+
+<form action="." method="GET">
+
+<textarea name="query" cols="60" rows="8"></textarea>
+
+<input type="submit" value="Submit" />
+
+</form>
+
+</p>
+
+<p><small>Note: tables and fields are case sensitive. Run "show tables" to see the list of
+
+available tables.</small>
+
+</p>
+
+
+
+
+
+</body>
+
+</html>
